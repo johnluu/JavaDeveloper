a58578d1516b199f4a71d4a221d6294a56fe7075
johnluu <johnluu44@gmail.com>
2018-03-30 16:22:05 -0400

    Add files via upload

--- Assignment 3/Assignment 3_3.sql
+++ Assignment 3/Assignment 3_3.sql
@@ -4,6 +4,7 @@ create table students
   last_name varchar(20),
   e_mail varchar(20),
   address varchar(20),
+  state VARCHAR(2),
   pin varchar(20),
   phone_no varchar(20),
   s_id varchar(5) primary key,
@@ -15,6 +16,7 @@ create table students
   subjects varchar(20)
 );
 
+
 create table courses(
 c_name varchar(20),
 c_code number(3) primary key,
@@ -23,6 +25,8 @@ c_duration number(2),
 c_fee number(4),
 c_description varchar(20)
 );
+
+
 
 create table student_courses(
 s_id varchar(5) ,
@@ -42,46 +46,144 @@ EXAM_DATE date,
 
 CONSTRAINT REG_NO_FK FOREIGN key(REG_NO) REFERENCES STUDENT_COURSES(REG_NO)
 );
-INSERT INTO STUDENTS(first_name,last_name,e_mail,s_id)
-VALUES('a','a', 'a', 's1001');
 
-INSERT INTO STUDENTS(first_name,last_name,e_mail,s_id)
-VALUES('b','b', 'b', 's1002');
 
-INSERT INTO STUDENTS(first_name,last_name,e_mail,s_id)
-VALUES('c','c', 'c', 's1003');
+create table CERTIFICATION(
+REG_NO varchar(5),
+C_CODE number(3),
+CERTIFICATE_REQUEST_ID NUMBER(3) PRIMARY KEY,
+CERTIFICATE_STATUS VARCHAR(7),
+ISSUE_DATE DATE,
 
-INSERT INTO courses(c_name,c_code,c_subjects)
-VALUES('course1','100', 'subject1');
+CONSTRAINT CERT_REG_FK FOREIGN key(REG_NO) REFERENCES STUDENT_COURSES(REG_NO),
+CONSTRAINT CERT_COURSE_FK FOREIGN key(C_CODE) REFERENCES COURSES(C_CODE)
+);
 
-INSERT INTO courses(c_name,c_code,c_subjects)
-VALUES('course2','101', 'subject2');
 
-INSERT INTO courses(c_name,c_code,c_subjects)
-VALUES('course2','102', 'subject2');
-
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1001','100', 'r1001');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1001','104', 'r1002');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1002','103', 'r1003');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1003','100', 'r1004');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1004','103', 'r1005');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1001','103', 'r1006');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1001','102', 'r1007');
-INSERT INTO student_courses(s_id,c_code,reg_no)
-VALUES('s1002','104', 'r1008');
+create table instructor
+(
+  first_name varchar(20),
+  last_name varchar(20),
+  address varchar(20),
+  state VARCHAR(2),
+  pin varchar(20),
+  instructor_id varchar(5) primary key,
+  date_of_birth date,
+  qualification varchar(2),
+  subjects varchar(20),
+  c_code number(3)
+);
 
 
 
+
+create table emp
+(
+  employee_id number(3) primary key,
+  first_name varchar(20),
+  last_name varchar(20),
+  phone_number number(4),
+  hire_date date,
+  salary number(9),
+  commision_pct number(3,2),
+  manager_id number(3),
+  job_id varchar(10),
+  department_id varchar(3)
+);
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('Ranjit','Chatterjee', 'r_chatt@aaa.com', '2/10 ashok vihar','WEST BENGAL','700101','9876578901','S1001',to_date('22-Apr-80','DD-MON-yy'),'BE',70,69,to_date('30-MAR-02','DD-MON-yy'),'MECHANICAL ENGG');
+
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('Kaushik','Bhatterjee', 'kaushik@bb.com', 'block A gagan vihar','WEST BENGAL','700096','9812345643','S1002',to_date('10-Aug-80','DD-MON-yy'),'BE',65,72,to_date('30-Apr-02','DD-MON-yy'),'CIVIL ENGG');
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('Kunal','GHOST', 'Kunal@gmail.com', '1A Shival APT','WEST BENGAL','700096','9678430987','S1003',to_date('12-dec-79','DD-MON-yy'),'BE',77,74,to_date('12-DEC-79','DD-MON-yy'),'MECHANICAL ENGG');
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('Pradeep','R', 'pradeep@gmail.com', '2B Trinity Tower','WEST BENGAL','700107','9234540987','S1004',to_date('3-may-81','DD-MON-yy'),'BE',78,70,to_date('22-sep-81','DD-MON-yy'),'ELECTRICAL ENGG');
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('RAKHI','R', 'TAKHI@gmail.com', '8/12C ASHOKA','WEST BENGAL','700109','9980768543','S1005',to_date('3-may-81','DD-MON-yy'),'BE',80,82,to_date('03-MAY-81','DD-MON-yy'),'ELECTRICAL ENGG');
+
+INSERT INTO STUDENTS(
+first_name,last_name,e_mail,address,state,pin,phone_no,s_id,date_of_birth,qualification,percent_10,percent_12,date_of_passing_12,SUBJECTS)
+VALUES('DRKJST','TSO', 'S_RAO@gmail.com', '22/10 A VISHAL APT','WEST BENGAL','700109',' ','S1006',to_date('3-may-81','DD-MON-yy'),'BE',75,80,to_date('03-MAY-81','DD-MON-yy'),'ELECTRICAL ENGG');
+
+
+INSERT INTO courses(c_name,c_code,c_subjects,C_DURATION,C_FEE,C_DESCRIPTION)
+VALUES('C Data Structure','100', 'C,DS',40,4000,'DETAILED C AND DATASTRUCTURE CONCEPTS');
+
+INSERT INTO courses(c_name,c_code,c_subjects,C_DURATION,C_FEE,C_DESCRIPTION)
+VALUES('HTML and JS','101', 'HTML,JS',20,3500,'DETAILED HTML AND JAVA SCRIPT');
+
+INSERT INTO courses(c_name,c_code,c_subjects,C_DURATION,C_FEE,C_DESCRIPTION)
+VALUES('JAVA','102', 'JAVA',90,6000,'CORE JAVA');
+
+INSERT INTO courses(c_name,c_code,c_subjects,C_DURATION,C_FEE,C_DESCRIPTION)
+VALUES('DATABASE','103', 'RDBMS, ORACLE',40,4000,'DATABASE CONCEPTS');
+
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1001','100', 'r1001' , 'COMPL');
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1001','102', 'r1002' , 'VALID');
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1002','100', 'r1003' , 'VALID');
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1003','103', 'r1004' , 'BREAK');
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1004','103', 'r1005' , 'COMPL');
+
+INSERT INTO student_courses(s_id,c_code,reg_no,COURSE_STATUS)
+VALUES('S1005','103', 'r1006' , 'COMPL');
+
+INSERT INTO student_score(Reg_No,score,exam_date)
+VALUES('r1001','70', to_date('12-jan-04', 'dd-mon-yy'));
+
+INSERT INTO student_score(Reg_No,score,exam_date)
+VALUES('r1005','80', to_date('15-sep-03', 'dd-mon-yy'));
+
+INSERT INTO student_score(Reg_No,score,exam_date)
+VALUES('r1006','78', to_date('12-dec-03', 'dd-mon-yy'));
+
+INSERT INTO CERTIFICATION(Reg_No,C_CODE,CERTIFI)
+VALUES('r1006','78', to_date('12-dec-03', 'dd-mon-yy'));
+
+INSERT INTO student_score(Reg_No,score,exam_date)
+VALUES('r1006','78', to_date('12-dec-03', 'dd-mon-yy'));
+
+INSERT INTO student_score(Reg_No,score,exam_date)
+VALUES('r1006','78', to_date('12-dec-03', 'dd-mon-yy'));
 
 INSERT INTO student_score(reg_no,score,exam_date)
 VALUES('r1001',70, TO_DATE('02-apr-80', 'DD-MON-yy'));
+
+INSERT INTO CERTIFICATION(reg_no,C_CODE,CERTIFICATE_REQUEST_ID,CERTIFICATE_STATUS,ISSUE_DATE)
+VALUES('r1001',100,101,'APPLIED', '');
+
+INSERT INTO CERTIFICATION(reg_no,C_CODE,CERTIFICATE_REQUEST_ID,CERTIFICATE_STATUS,ISSUE_DATE)
+VALUES('r1002',103,102,'ISSUED', TO_DATE('18-OCT-03', 'DD-MON-yy'));
+
+INSERT INTO CERTIFICATION(reg_no,C_CODE,CERTIFICATE_REQUEST_ID,CERTIFICATE_STATUS,ISSUE_DATE)
+VALUES('r1003',103,103,'ISSUED', TO_DATE('10-JAN-03', 'DD-MON-yy'));
+
+INSERT INTO EMP
+VALUES (10,'SMITH','TRENNA', 7890, TO_DATE('29-JAN-97'),20000,0.5,20,'SA_REP',10);
+
+
+
+
 
 /* QUESTION ONE */
 Select S.S_ID, S.FIRST_NAME,S.LAST_NAME, SC.C_CODE
--- Assignment 3/Assignment3_5.sql
+++ Assignment 3/Assignment3_5.sql
@@ -55,92 +55,125 @@ END;
 execute Factorial(10);
 */
  
+/*
+Question 4
 
-/* Question 4
 create or replace package emp_pack as
 
 procedure add_emp(
-  first_name in varchar,
-  last_name in varchar,
-  start_date in date,
-  title in varchar,
-  assigned_branch_id in number,
-  dept_id in number,
-  superior_emp_id in number);
+  add_EMPLOYEE_ID IN number,
+  add_first_name in varchar,
+  add_last_name in varchar,
+  add_PHONE_NUMBER IN NUMBER,
+  add_HIRE_date in date,
+  add_SALARY IN NUMBER,
+  add_COMMISION_PCT in number,
+  add_manager_id in number,
+  add_JOB_id in varchar,
+  add_DEPARTMENT_id in number);
+  
+  
+  
   
   procedure chg_emp(
-  chg_emp_id in number,
-  chg_first_name in varchar,
-  chg_last_name in varchar,
-  chg_start_date in date,
-  chg_title in varchar,
-  chg_assigned_branch_id in number,
-  chg_dept_id in number,
-  chg_superior_emp_id in number);
+  CHG_EMPLOYEE_ID IN number,
+  CHG_first_name in varchar,
+  CHG_last_name in varchar,
+  CHG_PHONE_NUMBER IN NUMBER,
+  CHG_HIRE_date in date,
+  CHG_SALARY IN NUMBER,
+  CHG_COMMISION_PCT in number,
+  CHG_MANAGER_ID in number,
+  CHG_JOB_id in varchar,
+  CHG_DEPARTMENT_id in number);
   
   
   
 end emp_pack;
 
 
-*/
-/*
+
+
 create or replace package body emp_pack as
 
 procedure add_emp(
-  first_name in varchar,
-  last_name in varchar,
-  start_date in date,
-  title in varchar,
-  assigned_branch_id in number,
-  dept_id in number,
-  superior_emp_id in number)
+  add_EMPLOYEE_ID IN number,
+  add_first_name in varchar,
+  add_last_name in varchar,
+  add_PHONE_NUMBER IN NUMBER,
+  add_HIRE_date in date,
+  add_SALARY IN NUMBER,
+  add_COMMISION_PCT in number,
+  add_manager_id in number,
+  add_JOB_id in varchar,
+  add_DEPARTMENT_id in number)
   as
 begin
 
-insert into employee (emp_id,first_name,last_name,start_date,title,assigned_branch_id,dept_id,SUPERIOR_EMP_ID)
-values((SELECT max(emp_id)+1 FROM employee), first_name,last_name,TO_DATE(start_date, 'DD-MON-yy'),title,assigned_branch_id,dept_id,SUPERIOR_EMP_ID);
+INSERT INTO EMP
+VALUES(
+  add_EMPLOYEE_ID,
+  add_first_name ,
+  add_last_name ,
+  add_PHONE_NUMBER ,
+  add_HIRE_date ,
+  add_SALARY ,
+  add_COMMISION_PCT,
+  add_manager_id ,
+  add_JOB_id ,
+  add_DEPARTMENT_id 
+);
+
+
 
 end add_emp;
 
 
 procedure chg_emp(
-  chg_emp_id in number,
+  chg_EMPLOYEE_ID IN number,
   chg_first_name in varchar,
   chg_last_name in varchar,
-  chg_start_date in date,
-  chg_title in varchar,
-  chg_assigned_branch_id in number,
-  chg_dept_id in number,
-  chg_superior_emp_id in number)
+  chg_PHONE_NUMBER IN NUMBER,
+  chg_HIRE_date in date,
+  chg_SALARY IN NUMBER,
+  chg_COMMISION_PCT in number,
+  chg_manager_id in number,
+  chg_JOB_id in varchar,
+  chg_DEPARTMENT_id in number)
   as
 begin
 
-update employee
+update EMP
 set
-first_name = chg_first_name,
-last_name = chg_last_name,
-start_date = TO_DATE(chg_start_date, 'DD-MON-yy'),
-title = chg_title,
-assigned_branch_id = chg_assigned_branch_id,
-dept_id = chg_dept_id,
-SUPERIOR_EMP_ID = chg_SUPERIOR_EMP_ID
-where emp_id = chg_emp_id;
+  first_name = chg_first_name,
+  last_name = chg_last_name,
+  phone_number = chg_PHONE_NUMBER,
+  hire_date = chg_HIRE_date,
+  salary = chg_SALARY,
+  commision_pct = chg_COMMISION_PCT,
+  manager_id = chg_manager_id,
+  job_id = chg_JOB_id,
+  department_id = chg_DEPARTMENT_id
+where employee_id = chg_employee_id;
 
 end chg_emp;
-
 
 end emp_pack;
 
 */
 
 /* EXECUTE PACKAGES*/
-begin
-EMP_PACK.ADD_EMP('people','places','04-jan-05','officer',2,2,2);
-end;
 
 
 begin
-EMP_PACK.CHG_EMP(21,'places','people','04-jan-05','officer',2,2,2);
+EMP_PACK.ADD_EMP(15,'SMITH','TRENNA', 7890,'29-JAN-97',20000,0.5,20,'SA_REP',10);
+END;
+
+begin
+EMP_PACK.chg_EMP(15,'Samantha','TRENNA', 7890,'29-JAN-97',20000,0.5,20,'SA_REP',10);
 end;
+
+alter table emp rename
+column Managager_id to manager_id;
+
 
