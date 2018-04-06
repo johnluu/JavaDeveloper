fa7145fcfd3afd765bfbc9e873e57c43a6e523ee
johnluu <johnluu44@gmail.com>
2018-04-06 14:15:09 -0400

    Add files via upload

---
+++ Assignment6_1/src/ArrList.java
@@ -0,0 +1,135 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrList {
+
+
+		
+	public static ArrayList<String> createArray(String ... s){
+		
+		ArrayList<String> arr = new ArrayList<String>();
+		
+		for(String c:s)
+			arr.add(c);	
+		return arr;
+		
+	}
+	
+	
+	public static void iterate(ArrayList<String> arr){
+		
+
+		for(int i = 0; i < arr.size() ; i ++)
+			System.out.println("Today's Color is " + arr.get(i));
+	}
+		
+
+	public static void update(int pos, String text, ArrayList<String> arr){
+		
+
+		arr.set(4, text);
+
+	}
+	
+	
+	public static void removeItem(int pos,ArrayList<String> arr ) {
+		
+		arr.remove(pos);
+		
+		return;
+	}
+	
+	public static int searchArr(String s,ArrayList<String> arr ) {
+		
+		for(int i = 0; i < arr.size(); i++)
+			if(arr.get(i).equals(s))
+				return i;
+			
+		
+		return -1;
+	}
+	
+	
+	public static void sortArr(ArrayList<String> arr ) {
+		
+		Collections.sort(arr);
+				
+		
+		
+		return;
+		
+	}
+
+	public static void reverse(ArrayList<String> arr) {
+		
+
+		
+		Collections.reverse(arr);
+		
+		return;
+	}
+	
+	public static ArrayList<String> copyArr(ArrayList<String> arr, ArrayList<String> copyarr) {
+		
+		for(String s: arr)
+			copyarr.add(s);
+		
+		
+		return copyarr;
+	}
+
+	
+	public static void compare(ArrayList<String> arr1, ArrayList<String> arr2) {
+		
+		for(String s: arr2)
+			if(arr1.contains(s))
+				System.out.println(s + " Was found in both lists ");
+		
+		
+		return;
+	}
+
+	
+	public static void append(ArrayList<String> arr1, ArrayList<String> arr2) {
+		
+		
+
+				
+		for(String s: arr2)
+		arr1.add(s);
+				
+		
+		
+		return;
+	}
+	
+
+
+	public static void loop(ArrayList<String> arr){
+		
+
+		for(String s:arr)
+			System.out.println(s);
+	}
+		
+
+
+		
+	public static ArrayList<String> ArraytoList(String[] arr){
+		
+		ArrayList<String> arrList = new ArrayList<String>();
+		
+		for(String s: arr)
+			arrList.add(s);
+
+		return arrList;
+	}
+		
+
+	
+
+	
+	
+
+}
+	
---
+++ Assignment6_1/src/testCase.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+public class testCase {
+
+	public static void main(String[] args) {
+		
+		///////Question 1 and 2///////////
+		////////////////////////////////////		
+		System.out.println("Create and Iterate List");
+		ArrayList<String> arr =ArrList.createArray("Red","Green","Orange","White","Black");
+		ArrList.iterate(arr);
+		System.out.println();
+
+		///////Question 3///////////
+		/////////////////////////////////////		
+		System.out.println("Updating an List");
+		ArrList.update(4, "Magenta", arr);
+		ArrList.iterate(arr);
+		System.out.println();
+	
+		///////Question 4///////////
+		/////////////////////////////////////
+		System.out.println("Removing an Item");
+		ArrList.removeItem(3, arr);
+		ArrList.iterate(arr);
+		System.out.println();
+		
+		///////Question 5///////////
+		////////////////////////////////////		
+		System.out.println("Searching an item");
+		int position = ArrList.searchArr("Green", arr);
+		System.out.println("The position was found in List[" + position + "]");
+		System.out.println();
+		
+		///////Question 6///////////
+		/////////////////////////////////////
+		System.out.println("Sorting Array");
+		ArrList.sortArr(arr);
+		ArrList.iterate(arr);
+		System.out.println();
+	
+		
+		///////Question 7///////////
+		////////////////////////////////////////////
+		System.out.println("Reverse");
+		ArrList.reverse(arr);
+		ArrList.iterate(arr);
+		System.out.println();
+
+
+
+		///////Question 8///////////
+		////////////////////////////////////////////
+		System.out.println("Copying");
+		ArrayList<String> copyarr = new ArrayList<String>();
+		ArrList.copyArr(arr, copyarr);
+		ArrList.iterate(copyarr);
+		System.out.println();
+
+
+		///////Question 9///////////
+		////////////////////////////////////////////
+		System.out.println("Compare");
+		copyarr.remove(2); //Removing Magenta
+		ArrList.compare(arr,copyarr);
+		System.out.println();
+
+
+		///////Question 10///////////
+		////////////////////////////////////////////
+		System.out.println("Appending");
+		ArrayList<String> arr2 = ArrList.createArray("Black","Chocolate","Brown");
+		ArrList.append(arr,arr2);
+		ArrList.iterate(arr);
+		System.out.println();
+		
+
+		
+		
+		///////Question 11 and 12///////////
+		////////////////////////////////////////////
+		System.out.println("Converting");
+		String[] array = {"Cows","Chickens","Sheeps"};
+		ArrayList<String> arr3 = ArrList.ArraytoList(array);
+		ArrList.loop(arr3);
+		System.out.println();
+		
+		
+	
+}
+
+}
