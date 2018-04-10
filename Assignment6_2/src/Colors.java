1e08af1e4a8dd83f5b48d22bfcc9143fcfc2ede2
johnluu <johnluu44@gmail.com>
2018-04-10 15:51:57 -0400

    Add files via upload

--- Assignment6_2/src/Colors.java
+++ Assignment6_2/src/Colors.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Colors {
 
@@ -22,10 +24,10 @@ public class Colors {
 		for(String key: map.values())
 			System.out.print(key + " ");
 		System.out.println();
-/////////////////////////////////////////
-		//////////////////////////////////
-		
+		////////////////////////////////
+
 		map.put(1 , "Brown");
+		//////////The value for key 1 was overwritten because it can only store one value
 		
 		
 		System.out.println("Keys");
@@ -37,9 +39,22 @@ public class Colors {
 		for(String key: map.values())
 			System.out.print(key + " ");
 		System.out.println();
+
 		
-		Object values;
-		Object keys;
+		System.out.println("Iterating");
+		Iterator<Map.Entry<Integer,String >> itr = map.entrySet().iterator();
+		
+		while(itr.hasNext())
+		{	Map.Entry<Integer, String> entry = itr.next();
+			System.out.println("Item" + entry.getKey() + " = " + entry.getValue());
+		}
+		
+		System.out.println("For Each Loop");
+		for(int key: map.keySet() )
+			System.out.println("Item " + key + " "+ map.get(key));
+		
+		
+		System.out.println("Built in for each");
 		map.forEach((k,v) ->
 		{
 			System.out.println("Item "+ k + " = " +v);
