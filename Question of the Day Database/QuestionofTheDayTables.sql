c27561a9d6389ef94b66357a2197a321d8ad0425
johnluu <johnluu44@gmail.com>
2018-05-11 16:35:12 -0400

    Add files via upload

--- Question of the Day Database/QuestionofTheDayTables.sql
+++ Question of the Day Database/QuestionofTheDayTables.sql
@@ -1,3 +1,15 @@
+select 'drop table ', table_name, 'cascade constraints;' from user_tables;
+drop table 	COMMENTS	cascade constraints;
+drop table 	USERS	cascade constraints;
+drop table 	NEW_QUESTIONS	cascade constraints;
+drop table 	NEW_OPTIONS	cascade constraints;
+drop table 	QUESTIONS	cascade constraints;
+drop table 	USER_WATCHING	cascade constraints;
+drop table 	OPTIONS	cascade constraints;
+drop table 	USER_CHOICES	cascade constraints;
+
+
+
 CREATE TABLE USERS (
 
   USER_ID numeric generated always as identity(start with 1 increment by 1) not null,
@@ -12,42 +24,42 @@ CREATE TABLE USERS (
 
 CREATE TABLE NEW_QUESTIONS(
 
-  NEW_QUESTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
+  QUESTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
   USER_ID NUMERIC,
   QUESTION_TEXT VARCHAR(255) NOT NULL,
   WATCHES NUMERIC DEFAULT 0,
   QUESTION_DATE DATE,
   
-  constraint NEWQUESTION_PK primary key (NEW_QUESTION_ID),
+  constraint NEWQUESTION_PK primary key (QUESTION_ID),
   FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID)
   
 );
 
 CREATE TABLE NEW_OPTIONS(
 
-  NEW_OPTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
-  NEW_QUESTION_ID NUMERIC,
+  OPTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
+  QUESTION_ID NUMERIC,
   OPTION_TEXT VARCHAR(20),
   
-  constraint NEWOPTION_PK primary key (NEW_OPTION_ID),
-  FOREIGN KEY (NEW_QUESTION_ID) REFERENCES NEW_QUESTIONS(NEW_QUESTION_ID)
+  constraint NEW_OPTION_PK primary key (OPTION_ID),
+  FOREIGN KEY (QUESTION_ID) REFERENCES NEW_QUESTIONS(QUESTION_ID)
 
 );
 
 CREATE TABLE USER_WATCHING(
 
-  NEW_QUESTION_ID NUMERIC,
+  QUESTION_ID NUMERIC,
   USER_ID NUMERIC,
-  WATCHING number(1),
+  WATCHING number(1) default 1,
  
-  CONSTRAINT USER_WATCHING_PK PRIMARY KEY (NEW_QUESTION_ID,USER_ID),
+  CONSTRAINT USER_WATCHING_PK PRIMARY KEY (QUESTION_ID,USER_ID),
   FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID),
-  FOREIGN KEY (NEW_QUESTION_ID) REFERENCES NEW_QUESTIONS(NEW_QUESTION_ID)
+  FOREIGN KEY (QUESTION_ID) REFERENCES NEW_QUESTIONS(QUESTION_ID)
 );
 
 CREATE TABLE QUESTIONS(
 
-  QUESTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
+  QUESTION_ID numeric not null,
   USER_ID NUMERIC not null,
   QUESTION_TEXT VARCHAR(255) NOT NULL,
   QUESTION_SCORE NUMERIC DEFAULT 0,
@@ -61,7 +73,7 @@ CREATE TABLE QUESTIONS(
 
 CREATE TABLE OPTIONS(
 
-  OPTION_ID numeric generated always as identity(start with 1 increment by 1) not null,
+  OPTION_ID numeric not null,
   QUESTION_ID NUMERIC not null,
   OPTION_TEXT VARCHAR(20),
   OPTION_SCORE numeric DEFAULT 0,
@@ -75,7 +87,7 @@ CREATE TABLE USER_CHOICES(
 
   QUESTION_ID NUMERIC not null,
   USER_ID NUMERIC not null,
-  OPTION_ID NUMERIC not null,
+  OPTION_ID NUMERIC default 1,
  
   CONSTRAINT USER_CHOICES_PK PRIMARY KEY (QUESTION_ID,USER_ID),
   FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID),
@@ -96,14 +108,10 @@ CREATE TABLE COMMENTS(
   FOREIGN KEY (QUESTION_ID) REFERENCES QUESTIONS(QUESTION_ID)
 );
 
-/*
-select 'drop table ', table_name, 'cascade constraints;' from user_tables;
-drop table 	COMMENTS	cascade constraints;
-drop table 	USERS	cascade constraints;
-drop table 	NEW_QUESTIONS	cascade constraints;
-drop table 	NEW_OPTIONS	cascade constraints;
-drop table 	QUESTIONS	cascade constraints;
-drop table 	USER_WATCHING	cascade constraints;
-drop table 	OPTIONS	cascade constraints;
-drop table 	USER_CHOICES	cascade constraints;
-*/
+
+
+
+
+
+
+
